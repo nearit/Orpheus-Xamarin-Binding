720795b22b5cbc0249429e88baa5f14f4594d461
Cattaneo Stefano <stefano@nearit.com>
2019-06-11 18:17:24 +0200

    chore(sample): add sample project

---
+++ OrpheusSample/EmptyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace OrpheusSample
+{
+    public class EmptyClass: IT.Near.Sdk.Jsonapiparser.Resource
+    {
+        public EmptyClass()
+        {
+        }
+
+        public string Name 
+        {
+            set;
+            get;
+        }
+    }
+}
